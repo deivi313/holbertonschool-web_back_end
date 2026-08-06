5074eea17c50ecd907b8dc82f882c236b46309b2
deivi313 <deiviboja@gmail.com>
2026-08-06 18:51:23 +0200

    task 1

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-  new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (success) {
-      return resolve({ status: 200, body: 'Success' })
+      resolve({ status: 200, body: 'Success' });
     } else {
-      return reject(new Error("The fake API is not working currently"))
+      reject(new Error('The fake API is not working currently'));
     }
-  })
+  });
 }
